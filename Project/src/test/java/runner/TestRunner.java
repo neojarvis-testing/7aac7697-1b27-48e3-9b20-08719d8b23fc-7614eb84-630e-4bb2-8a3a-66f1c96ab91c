5d887f657ad5c47086d5adb2e767387cbb6fc741
tapaswinipsnist <tapaswinip1804@gmail.com>
2025-04-21 18:27:33 +0000

    firstday commit tapaswini

--- Project/src/test/java/runner/TestRunner.java
+++ Project/src/test/java/runner/TestRunner.java
@@ -1,5 +1,12 @@
 package runner;
 
-public class TestRunner {
+import org.testng.annotations.Test;
 
+import utils.LoggerHandler;
+
+public class TestRunner {
+@Test
+public void example(){
+    LoggerHandler.info("Sucuccess");
+}
 }
