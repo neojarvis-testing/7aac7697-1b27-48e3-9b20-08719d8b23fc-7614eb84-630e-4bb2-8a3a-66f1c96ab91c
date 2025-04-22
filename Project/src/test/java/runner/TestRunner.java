30e2a5b3dd211c18ed171f11c35faca2923c298b
Viny7816 <vinayvin2575@gmail.com>
2025-04-22 17:28:59 +0000

    prefinal commit vinay

--- Project/src/test/java/runner/TestRunner.java
+++ Project/src/test/java/runner/TestRunner.java
@@ -3,30 +3,34 @@ package runner;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
 import pages.HomePageActions_Shambhavi01;
 import pages.HomePageActions_Vinay01;
+import pages.HomePageActions_Vinay02;
 import pages.HomePageActions_tapaswini01;
 import pages.HomePageActions_arun01;
 import pages.HomePageActions_arun02;
 import utils.Base;
+import utils.Reporter;
 import pages.HomePageActions_Deekshitha01;
 
 public class TestRunner extends Base {
+    static ExtentReports reports;
+    static ExtentTest test;
     @BeforeMethod
     public void launchRelianceJewels() {
-        openBrowser();
+         openBrowser();
+        reports=Reporter.generateTestReport("Reliance_Report");
+        test=reports.createTest("Reliance_TestFile");
     }
     @Test
     public void reliance() {
         HomePageActions_Shambhavi01 hp = new HomePageActions_Shambhavi01();
         hp.testcase1();
     }
-
-    @AfterMethod
-    public void closeRelianceJewels() {
-        driver.quit();
-    }
-}
     @Test
     public void Test1() {
         HomePageActions_Deekshitha01 hp = new HomePageActions_Deekshitha01();
@@ -34,9 +38,15 @@ public class TestRunner extends Base {
     }
     @Test
     public void testCase1() {
-        HomePageActions_Vinay01 obj1 = new HomePageActions_Vinay01();
+        HomePageActions_Vinay01 obj1 = new HomePageActions_Vinay01(test);
         obj1.test1();
     }
+    @Test
+    public void testCase2() {
+        HomePageActions_Vinay02 obj1 = new HomePageActions_Vinay02(test);
+        obj1.test2();
+    }
+    @Test
     public void vivahamTest() {
         HomePageActions_tapaswini01 vivahamPageActions = new HomePageActions_tapaswini01(driver);
         vivahamPageActions.verifyLogo();
@@ -52,20 +62,22 @@ public class TestRunner extends Base {
         vivahamPageActions.switchToMainWindow();
         vivahamPageActions.scrollToDelhiText();
         vivahamPageActions.clickDelhi();
-        vivahamPageActions.verifyDelhiPage();
-        vivahamPageActions.verifySortByText();
+       // vivahamPageActions.verifyDelhiPage();
+       // vivahamPageActions.verifySortByText();
     }
-    public void testCase1() {
+    @Test
+    public void testCase1Arun() {
         HomePageActions_arun01 obj1 = new HomePageActions_arun01();
         obj1.test1();
     }
     @Test
-    public void testCase2() {
+    public void testCase2Arun() {
         HomePageActions_arun02 obj2 = new HomePageActions_arun02();
         obj2.test2();
     }
     @AfterMethod
     public void closeRelianceJewels() {
         driver.quit();
+        reports.flush();
     }
 }
